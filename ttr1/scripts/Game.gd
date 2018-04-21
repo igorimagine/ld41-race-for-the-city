668eae89bdcd981fea9c9b60954f370d5ad0d7cb
Igor Durbek <igor.imagine.d@gmail.com>
2018-04-21 10:52:53 +0200

    Too many buildings are being spawned

--- ttr1/scripts/Game.gd
+++ ttr1/scripts/Game.gd
@@ -1,7 +1,6 @@
 extends Node
 
 onready var car_area = $CarSpatial/CarCube/Area
-#onready var car_cube = $CarSpatial/CarCube
 onready var car_spatial = $CarSpatial
 onready var road_top_area = $RoadTopCube/Area
 onready var gold_label = $GoldTextureRect/GoldLabel
@@ -19,19 +18,34 @@ onready var th_ui_area_hit = false
 onready var yellow_cube_spatial = $YellowCubeSpatial
 onready var th_building = false
 onready var yellow_cube_scene = load("res://scenes/YellowCube.tscn")
-onready var building = null
+onready var last_building_number = 0
+onready var building_number = 0
+onready var town_halls = []
 
 func _ready():
 	randomize()
 	_move_reward()
-	#building = yellow_cube_scene.instance()
-	#self.add_child(building)
 	pass
+
+func _do_lmb():
+	var mouse_pos = get_viewport().get_mouse_position()
+	var ray_origin = main_camera.project_ray_origin(mouse_pos)
+	var ray_direction = main_camera.project_ray_normal(mouse_pos)
+	var from = ray_origin
+	var to = ray_origin + ray_direction * 1000.0
+	var space_state = ground_top.get_world().direct_space_state
+	var hit = space_state.intersect_ray(from, to)
+	if hit.size() != 0:
+		if hit.collider_id == th_ui_area.get_instance_id():
+			th_ui_area_hit = true
+			building_number += 1
 
 func _process(delta):
 	reward_delay -= 1
+	var building = null
+	if (last_building_number > 0):
+		building = town_halls[town_halls.size() - 1]
 	if car_area.overlaps_area(road_top_area):
-		#prints("over")
 		gold += 0.1
 		pass
 	if car_area.overlaps_area(reward_basic_cube_area):
@@ -52,9 +66,6 @@ func _process(delta):
 	if Input.is_action_just_pressed("LMB"):
 		_do_lmb()
 	if th_ui_area_hit:
-#		var mouse_pos = get_viewport().get_mouse_position()
-#		prints("trans", yellow_cube_spatial.get_translation())
-#		yellow_cube_spatial.set_translation(Vector3(mouse_pos.x, yellow_cube_spatial.get_translation().y, mouse_pos.y)) #ovo zadnje je z
 		var mouse_pos = get_viewport().get_mouse_position()
 		var ray_origin = main_camera.project_ray_origin(mouse_pos)
 		var ray_direction = main_camera.project_ray_normal(mouse_pos)
@@ -63,9 +74,16 @@ func _process(delta):
 		var space_state = ground_top.get_world().direct_space_state
 		var hit = space_state.intersect_ray(from, to)
 		if hit.size() != 0:
-			if building == null:
+			#if building == null:
+#				building = yellow_cube_scene.instance()
+#				self.add_child(building)
+#				town_halls.append(building)
+			#if (building_number == 0 || (building_number == last_building_number)):
+			if building_number == (last_building_number + 1):
 				building = yellow_cube_scene.instance()
 				self.add_child(building)
+				town_halls.append(building)
+				#last_building_number += 1
 			building.set_translation(Vector3(hit.position.x, 0, hit.position.z))
 			th_building = true
 			pass
@@ -75,6 +93,8 @@ func _process(delta):
 		if th_building:
 			th_building = false
 			th_ui_area_hit = false
+			last_building_number += 1
+			#th_building_placed = true ???
 			pass
 		pass
 	if Input.is_action_just_pressed("E"):
@@ -92,15 +112,3 @@ func _move_reward():
 	var z_range = rand_range(-4.0, 5.1)
 	reward_spatial.set_translation(Vector3(x_range, reward_spatial.translation.y, z_range))
 	pass
-	
-func _do_lmb():
-	var mouse_pos = get_viewport().get_mouse_position()
-	var ray_origin = main_camera.project_ray_origin(mouse_pos)
-	var ray_direction = main_camera.project_ray_normal(mouse_pos)
-	var from = ray_origin
-	var to = ray_origin + ray_direction * 1000.0
-	var space_state = ground_top.get_world().direct_space_state
-	var hit = space_state.intersect_ray(from, to)
-	if hit.size() != 0:
-		if hit.collider_id == th_ui_area.get_instance_id():
-			th_ui_area_hit = true
